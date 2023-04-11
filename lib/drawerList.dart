a5560ddf3bac7adb3b6707a58c52847c809b2845
AndersonBenjamin <31894451+AndersonBenjamin@users.noreply.github.com>
2023-04-11 00:53:02 -0300

    Inclusao de pagina de cadastra drawerList

---
+++ lib/drawerList.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:relogio_ponto/pages/home_page.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              builderHeader(context),
+              builderMenuItems(context),
+            ],
+          ),
+        ),
+      );
+}
+
+Widget builderHeader(BuildContext context) => Container(
+      color: Colors.deepPurple,
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+      ),
+      child: Column(children: const [
+        CircleAvatar(
+          radius: (52),
+          backgroundImage: AssetImage(
+            'lib/images/pessoal.jpeg',
+          ),
+        ),
+        SizedBox(height: 12),
+        Text(
+          'Anderson',
+          style: TextStyle(fontSize: 28, color: Colors.white),
+        ),
+        Text(
+          'anderson@teste.com',
+          style: TextStyle(fontSize: 16, color: Colors.white),
+        ),
+      ]),
+    );
+Widget builderMenuItems(BuildContext context) => Container(
+      padding: const EdgeInsets.all(24),
+      child: Wrap(
+        runSpacing: 16, // vertical space
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            onTap: () => Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.favorite_border),
+            title: const Text('Favorites'),
+            onTap: () => Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.workspaces_outline),
+            title: const Text('WorkFlow'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.update),
+            title: const Text('Updatte'),
+            onTap: () {},
+          ),
+          const Divider(
+            color: Colors.black54,
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_tree_outlined),
+            title: const Text('Plugins'),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications_outlined),
+            title: const Text('Notifications'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import '../drawerList.dart';
+
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
@@ -14,12 +16,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black, actions: [
+      appBar: AppBar(backgroundColor: Colors.deepPurple, actions: [
         IconButton(
           onPressed: signUserOut,
           icon: Icon(Icons.logout),
         ),
       ]),
+      drawer: NavigationDrawer(),
       body: Center(
           child: Text(
         "LOGGED IN! " + user.email!,
